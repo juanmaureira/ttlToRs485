ce343b439c2b0b07b9aeb99e7f1692f433d77d21
elocker <elocker@elocker.cl>
2019-08-30 13:22:15 -0400

    ACtualización

--- pruebaRS-485.ino
+++ pruebaRS-485.ino
@@ -1,19 +1,21 @@
 
 # include <SoftwareSerial.h>
+// PIN 9 -> RE/DE MAX485
 # define ENRS485 9
-
-SoftwareSerial mySerial(10, 11); // RX, TX
+// PIN 10 -> RX
+// PIN 11 -> TX
+SoftwareSerial mySerial(10, 11); 
 
 void setup() {
-  
-  //Puerto serial USB de ARDUINO
+  // Puerto serial USB de ARDUINO
   Serial.begin(57600);
   while (!Serial);
   // Puerto TTL <--> RS-485
   mySerial.begin(19200);
+  // 
   pinMode(ENRS485, OUTPUT);
   digitalWrite(ENRS485, LOW);
-  mySerial.write("Test RS-485\r\n");
+  Serial.write("Test RS-485\r\n");
   
 }
 
